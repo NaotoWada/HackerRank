f7a9cf51d9b0210fca242dd85c7c2d864041e1b0
naoto <naoto.oct25.g@gmail.com>
2019-10-04 01:05:09 +0900

    add:kodamanコン追加
    まだ未提出なので、時間があるときやる

---
+++ src/main/kodaman/A_Imaging.java
@@ -0,0 +1,10 @@
+package main.kodaman;
+
+import java.util.Scanner;
+
+public class A_Imaging {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
+}
